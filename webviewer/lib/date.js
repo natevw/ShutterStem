ba12d163d728275884376551971c351c25157818
Nathan Vander Wilt <nate@calftrail.com>
2010-11-23 22:29:18 -0800

    improved readme, some WebKit multitouch experimentation (bzr r125)

--- webviewer/lib/date.js
+++ webviewer/lib/date.js
@@ -7,16 +7,15 @@ function dateFromRFC3399(rfc3399) {
 		return new Date(time);
 	}
 	
+	// Nasty RFC 822–style dates are well supported. So we convert, e.g.:
+	// "2010-07-28T00:00:00.5Z" to "28 Jul 2010 00:00:00 GMT"
+	// "2010-07-28T00:00:00.5-07:00" to "28 Jul 2010 00:00:00 -0700"
+	// ...then add back millseconds
 	
-	/* The following is fairly insane approach, converting first to the deprecated date format recognition */
-	
-	// "2010-07-28T00:00:00.5Z" to "28 Jul 2010 00:00:01 GMT"
-	// "2010-07-28T00:00:00.5-07:00" to "28 Jul 2010 00:00:01 GMT"
-
 	parts = rfc3399.split("T");
 	date = parts[0];
 	time = parts[1];
-
+	
 	parts = date.split("-").map(function (n) {
 		return parseInt(n, 10);
 	});
@@ -25,17 +24,17 @@ function dateFromRFC3399(rfc3399) {
 		"May", "Jun", "Jul", "Aug",
 		"Sep", "Oct", "Nov", "Dec"];
 	date = [parts[2], MA[parts[1]], parts[0]].join(" ");
-
+	
 	time = time.replace("Z", " GMT");
 	time = time.replace(/([+-])(\d\d):(\d\d)$/, " $1$2$3");
 	parts = 0;
 	time = time.replace(/[0-9]{2}\.[0-9]+ /, function (m) {
 		var intSecs = parseInt(m);
 		parts = parseFloat(m) - intSecs;
-		var intSecs = intSecs.toFixed();
+		intSecs = intSecs.toFixed();
 		return (intSecs.length == 2) ? intSecs + " " : "0" + intSecs + " ";
 	});
-
+	
 	date = new Date(date + " " + time);
 	date.setMilliseconds(parts * 1000);
 	return date;
