49947b2d28f848db892b5692a42a3181d5291802
Nathan Vander Wilt <nate@calftrail.com>
2010-12-31 09:32:48 -0600

    added a bunch of notes (bzr r128), prepping to move over to git as repo (there are five lights)

--- webviewer/shows/photo_info.js
+++ webviewer/shows/photo_info.js
@@ -6,7 +6,7 @@ function (doc, req) {
 	mustache = require("vendor/couchapp/lib/mustache");
 	
 	provides("html", function () {
-		doc.source = JSON.stringify(doc);
+		doc.source = JSON.stringify(doc, null, 4);
 		doc.thumbnail = {"medium": path.doc(doc._id, "medium.jpg") };
 		if (!doc.name && doc.image && doc.image.original_filename) {
 			doc.name = doc.image.original_filename;
