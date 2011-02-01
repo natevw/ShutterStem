8f410d3d8bbe6ad46c8f9dcdf82ee43bbf75f9ce
Nathan Vander Wilt <nate@calftrail.com>
2011-01-31 23:14:08 -0800

    fleshed out import request processing (does nothing for now)

--- _attachments/scripts/import.py
+++ _attachments/scripts/import.py
@@ -2,6 +2,7 @@
 
 # see http://wiki.apache.org/couchdb/ExternalProcesses for configuration instructions
 
+import os
 import json
 
 class CouchExternal(object):
@@ -11,7 +12,10 @@ class CouchExternal(object):
         
         line = sys.stdin.readline()
         while line:
-            response = self.process(json.loads(line))
+            try:
+                response = self.process(json.loads(line))
+            except Exception:
+                response = {'code':500, 'json':{'error':True, 'reason':"Internal error processing request"}}
             sys.stdout.write("%s\n" % json.dumps(response))
             sys.stdout.flush()
             line = sys.stdin.readline()
@@ -21,7 +25,35 @@ class CouchExternal(object):
 
 
 class ShutterStemImporter(CouchExternal):
+    def __init__(self):
+        self.imports = {}
+    
     def process(self, req):
+        source_id = req['path'][2]
+        if req['method'] == 'POST':
+            token = req['query'].get('csrf_token', None)
+            helper = req['query'].get('helper_path', None)
+            if not token or not helper or not source_id:
+                return {'code':400, 'json':{'error':True, 'reason':"Required parameter(s) missing"}}
+            
+            # check that the request is not forged
+            with open(helper, 'r') as f:
+                first_chunk = f.read(4096)  # token required in first 4k
+                if first_chunk.find("<!-- SHUTTERSTEM-TOKEN(%s)TOKEN-SHUTTERSTEM -->" % token) == -1:
+                    raise Exception()
+            
+            if source_id in self.imports:
+                return {'code':409, 'json':{'error':True, 'reason':"An import is already in progress for this source"}}
+            
+            info = self.imports[source_id] = {}
+            info['folder'] = os.path.dirname(helper)
+            # TODO: start import of requested folder in background
+            return {'code':202, 'json':{'ok':True, 'message':"Import of '%s' has started." % info['folder']}}
+        
+        if req['method'] == 'GET':
+            # TODO: return status info (w/long polling?)
+            pass
+        
         return {'body': "<h1>Hello World!</h1>\n<pre>%s</pre>" % json.dumps(req, indent=4)}
         
 
