62e1e06e4830d9c6c5592e6424fb8d84dfacdf3d
Nathan Vander Wilt <nate@calftrail.com>
2011-02-01 21:24:39 -0800

    added csrf token generation to import utility generator

--- shows/source_utility.js
+++ shows/source_utility.js
@@ -1,12 +1,15 @@
 function (doc, req) {
     var ddoc = this;
     provides("html", function () {
-        var Mustache = require("lib/mustache");
-        
+        if (!req.query.random) {
+            // avoid caching of csrf_token (a bit overzealous...?)
+            return {code:400, body:"<h1>Bad Request</h1>You must include a random query parameter."};
+        }
         doc.database_url = "http://localhost:5984/" + req.info.db_name;
         doc.database_futon_url = "http://localhost:5984/_utils/database.html?" + req.info.db_name;
+        doc.csrf_token = '' + Math.round(Math.random() * 1e16) + req.uuid + Math.round(Math.random() * 1e16) + '';
         
-        var html = Mustache.to_html(ddoc.templates.source_utility, doc);
+        var html = require("lib/mustache").to_html(ddoc.templates.source_utility, doc);
         //return {headers:{'Content-Disposition':"attachment"}, body:html};
         return html;
     });
