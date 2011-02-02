bf3cb41aa1c48cf6aad62cfcda7c5aeed0fe1969
Nathan Vander Wilt <nate@calftrail.com>
2011-02-01 23:03:00 -0800

    shuffled around local-to-server iframe pieces, blended import status foundation into source utility

--- _attachments/scripts/import.py
+++ _attachments/scripts/import.py
@@ -4,6 +4,8 @@
 
 import os
 import json
+import uuid
+from time import sleep
 
 class CouchExternal(object):
     def run(self):
@@ -28,27 +30,35 @@ class ShutterStemImporter(CouchExternal):
     def __init__(self):
         self.imports = {}
     
-    def process(self, req):
+    def process_folder(self, req):
         source_id = req['path'][2]
-        if req['method'] == 'POST':
-            token = req['query'].get('csrf_token', None)
-            helper = req['query'].get('helper_path', None)
-            if not token or not helper or not source_id:
-                return {'code':400, 'json':{'error':True, 'reason':"Required parameter(s) missing"}}
-            
-            # check that the request is not forged
-            with open(helper, 'r') as f:
-                first_chunk = f.read(4096)  # token required in first 4k
-                if first_chunk.find("<!-- SHUTTERSTEM-TOKEN(%s)TOKEN-SHUTTERSTEM -->" % token) == -1:
-                    raise Exception()
-            
-            if source_id in self.imports:
-                return {'code':409, 'json':{'error':True, 'reason':"An import is already in progress for this source"}}
-            
-            info = self.imports[source_id] = {}
-            info['folder'] = os.path.dirname(helper)
-            # TODO: start import of requested folder in background
-            return {'code':202, 'json':{'ok':True, 'message':"Import of '%s' has started." % info['folder']}}
+        token = req['query'].get('token', None)
+        helper = req['query'].get('utility', None)
+        if not token or not helper or not source_id:
+            return {'code':400, 'json':{'error':True, 'reason':"Required parameter(s) missing"}}
+        
+        # check that the request is not forged
+        with open(helper, 'r') as f:
+            first_chunk = f.read(4096)  # token required in first 4k
+            if first_chunk.find("<!-- SHUTTERSTEM-TOKEN(%s)TOKEN-SHUTTERSTEM -->" % token) == -1:
+                raise Exception()
+        
+        if source_id in self.imports:
+            return {'code':409, 'json':{'error':True, 'reason':"An import is already in progress for this source"}}
+        
+        info = self.imports[source_id] = {}
+        info['token'] = uuid.uuid4().hex
+        info['folder'] = os.path.dirname(helper)
+        # TODO: start import of requested folder in background
+        return {'code':202, 'json':{'ok':True, 'message':"Import of '%s' may now start." % info['folder'], 'token':info['token']}}
+    
+    def process(self, req):
+        if req['method'] == 'POST' and req['path'][3] == "folder":
+            try:
+                return self.process_folder(req)
+            except Exception:
+                sleep(2.5)    # slow down malicious local scanning
+                return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
         
         if req['method'] == 'GET':
             # TODO: return status info (w/long polling?)
