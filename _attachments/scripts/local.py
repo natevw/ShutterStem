374a020da8e003c7532b44278a66d7e1e2a9a612
Nathan Vander Wilt <nate@calftrail.com>
2011-02-12 22:09:30 -0800

    a bit of renaming/reshuffling of import stuff

--- _attachments/scripts/local.py
+++ _attachments/scripts/local.py
@@ -9,9 +9,9 @@ from time import sleep
 import os
 import json
 
-class ImportManager(couch.External):
+class LocalHelper(couch.External):
     def __init__(self):
-        self.imports = {}
+        self.importers = {}
     
     def process_folder(self, req):
         source_id = req['path'][2]
@@ -26,13 +26,12 @@ class ImportManager(couch.External):
             if first_chunk.find("<!-- SHUTTERSTEM-TOKEN(%s)TOKEN-SHUTTERSTEM -->" % token) == -1:
                 raise Exception()
         
-        if source_id in self.imports:
+        if source_id in self.importers:
             return {'code':409, 'json':{'error':True, 'reason':"An import is already in progress for this source"}}
         
-        info = self.imports[source_id] = {}
         db_url = "http://%s/%s" % (req['headers']['Host'], req['info']['db_name'])
         folder = os.path.dirname(helper)
-        info['importer'] = Importer(db_url, source_id, folder)
+        self.importers[source_id] = Importer(db_url, source_id, folder)
         
         return {'code':202, 'json':{'ok':True, 'message':"Import of '%s' may now start." % folder}}
     
@@ -42,20 +41,20 @@ class ImportManager(couch.External):
         if not action or not source_id:
             return {'code':400, 'json':{'error':True, 'reason':"Required parameter(s) missing"}}
         
-        if source_id not in self.imports:
+        if source_id not in self.importers:
             return {'code':404, 'json':{'error':True, 'reason':"No import is in progress for this source"}}
         
-        info = self.imports[source_id]
+        importer = self.importers[source_id]
         
         if action == 'import':
-            info['importer'].begin()
+            importer.begin()
             return {'code':202, 'json':{'ok':True, 'message':"Import will proceed"}}
         elif action == 'cancel':
-            info['importer'].cancel()
+            importer.cancel()
             return {'code':202, 'json':{'ok':True, 'message':"Import is cancelling"}}
         elif action == 'finish':
-            info['importer'].finish()
-            del self.imports[source_id]
+            importer.finish()
+            del self.importers[source_id]
             return {'code':200, 'json':{'ok':True, 'message':"Import finished"}}
         
         return {'code':400, 'json':{'error':True, 'reason':"Unknown action"}}
@@ -80,15 +79,15 @@ class ImportManager(couch.External):
         #    return {'body': "<h1>Hello World!</h1>\n<pre>%s</pre>" % json.dumps(req, indent=4)}
         elif req['method'] == 'GET':
             source_id = req['path'][2]
-            if source_id not in self.imports:
+            if source_id not in self.importers:
                 return {'code':404, 'json':{'error':True, 'reason':"No import is in progress for '%s'." % source_id}}
             
-            status = self.imports[source_id]['importer'].status()
+            status = self.importers[source_id].status()
             return {'json':status}
         
         return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
         
 
 if __name__ == "__main__":
-    import_server = ImportManager()
-    import_server.run()
+    LocalHelper().run()
+
