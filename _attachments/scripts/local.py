0013f94cbd2c29d0c01b185f33d612534d3daa65
Nathan Vander Wilt <nate@calftrail.com>
2011-02-21 23:00:28 -0800

    overhauled local.py to better support actions other than import (e.g. split folder originals hosting into separate handler)

--- _attachments/scripts/local.py
+++ _attachments/scripts/local.py
@@ -15,7 +15,6 @@ class LocalHelper(couch.External):
     def __init__(self):
         self.importers = {}
     
-    
     @property
     def configfile(self):
         if os.path.exists(CONFIG):
@@ -33,72 +32,39 @@ class LocalHelper(couch.External):
     def configfile(self):
         os.remove(CONFIG)
     
-    def check_utility(self, utility_path, token):
-        with open(utility_path, 'r') as f:
+    def check_utility(self, utility_path, csrf_token):
+        with open(utility_path, 'rb') as f:
             first_chunk = f.read(4096)  # token required in first 4k
-            if first_chunk.find("<!-- SHUTTERSTEM-TOKEN(%s)TOKEN-SHUTTERSTEM -->" % token) == -1:
+            if first_chunk.find("<!-- SHUTTERSTEM-TOKEN(%s)TOKEN-SHUTTERSTEM -->" % csrf_token) == -1:
                 raise Exception()
     
-    def process_folder(self, req):
-        source_id = req['path'][2]
-        token = req['query'].get('token', None)
-        helper = req['query'].get('utility', None)
-        if not source_id or not token or not helper:
-            return {'code':400, 'json':{'error':True, 'reason':"Required parameter(s) missing"}}
+    def process(self, req):
+        _db_name, _this_external, action = req['path'][:3]
+        action_subpath = req['path'][3:]
+        req['database_url'] = "http://%s/%s" % (req['headers']['Host'], req['info']['db_name'])
         
-        # check that the request is not forged
-        self.check_utility(helper, token)
-        
-        folder, name = os.path.split(helper)
-        config = self.configfile
-        folders = config.setdefault('sources', {}).setdefault(source_id, {}).setdefault('folders', {})
-        
-        allow_originals = req['query'].get('allow_originals', 'true' if (folder in folders) else None)
-        if allow_originals is not None:
-            if allow_originals == 'true':
-                # update or set folder utility information
-                folders[folder] = {'utility':name, 'token':token}
-                message = "Originals may be hosted from '%s' while import utility remains in place." % folder
+        utility_path, csrf_token = req['query'].get('utility', None), req['query'].get('token', None)
+        if utility_path and csrf_token:
+            try:
+                self.check_utility(utility_path, csrf_token)
+            except Exception:
+                sleep(2.5)    # slow down malicious local scanning
+                return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
             else:
-                del folders[folder]
-                message = "Originals will NOT be hosted from '%s'." % folder
-            self.configfile = config
-            if 'allow_originals' in req['query']:
-                return {'code':200, 'json':{'ok':True, 'message':message}}
+                folder, name = os.path.split(utility_path)
+                req['utility'] = {'path':utility_path, 'token':csrf_token, 'folder':folder, 'name':name}
+        elif req['method'] != 'GET':
+            return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
         
-        originals = folder in folders
-        if source_id in self.importers:
-            return {'code':409, 'json':{'error':True, 'reason':"An import is already in progress for this source", 'originals':originals}}
-        else:
-            self.importers[source_id] = Importer("http://%s/%s" % (req['headers']['Host'], req['info']['db_name']), source_id, folder)
-            return {'code':202, 'json':{'ok':True, 'message':"Import of '%s' may now start." % folder, 'originals':originals}}
-    
-    def process_action(self, req):
-        action = req['path'][3]
-        source_id = req['path'][2]
-        if not action or not source_id:
-            return {'code':400, 'json':{'error':True, 'reason':"Required parameter(s) missing"}}
+        processor = getattr(self, 'process_%s_%s' % (action, req['method']), None)
+        if processor:
+            return processor(req, action_subpath)
         
-        if source_id not in self.importers:
-            return {'code':404, 'json':{'error':True, 'reason':"No import is in progress for this source"}}
-        
-        importer = self.importers[source_id]
-        
-        if action == 'import':
-            importer.begin()
-            return {'code':202, 'json':{'ok':True, 'message':"Import will proceed"}}
-        elif action == 'cancel':
-            importer.cancel()
-            return {'code':202, 'json':{'ok':True, 'message':"Import is cancelling"}}
-        elif action == 'finish':
-            importer.finish()
-            del self.importers[source_id]
-            return {'code':200, 'json':{'ok':True, 'message':"Import finished"}}
-        
-        return {'code':400, 'json':{'error':True, 'reason':"Unknown action"}}
+        #return {'body': "<h1>Hello World!</h1>\n<pre>%s</pre>" % json.dumps(req, indent=4)}
+        return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
     
     
-    def process_image(self, req):
+    def process_image_GET(self, req):
         image_doc = self.db.read(req.path[2])
         image_path_info = image_doc['identifiers']['relative_path']
         source_id = image_path_info['source']['_id']
@@ -118,35 +84,59 @@ class LocalHelper(couch.External):
                 break
         
         return {'code':404, 'json':{'error':True, 'reason':"No original image could be found"}}
-        
     
-    def process(self, req):
-        if req['method'] == 'POST' and req['path'][3] == "folder":
-            try:
-                return self.process_folder(req)
-            except Exception:
-                sleep(2.5)    # slow down malicious local scanning
-                return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
+    
+    def process_import_GET(self, req, subpath):
+        source_id = subpath[0]
+        importer = self.importers.get(source_id, None)
+        if importer:
+            return {'json':importer.status()}
+        else:
+            return {'code':404, 'json':{'error':True, 'reason':"No import is in progress for '%s'." % source_id}}
+    
+    def process_import_POST(self, req, subpath):
+        source_id, action = subpath[:2]
+        importer = self.importers.get(source_id, None)
+        if not importer and action != 'create':
+            return {'code':404, 'json':{'error':True, 'reason':"No import is in progress for '%s'." % source_id}}
+        elif action == 'create' and importer:
+            return {'code':409, 'json':{'error':True, 'reason':"An import is already in progress for this source"}}
         
-        elif req['method'] == 'POST' and req['path'][3] in ('import', 'cancel', 'finish'):
-            try:
-                return self.process_action(req)
-            except Exception:
-                sleep(0.5)
-                return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
+        if action == 'create':
+            self.importers[source_id] = Importer(req['database_url'], source_id, req['utility']['folder'])
+            return {'code':202, 'json':{'ok':True, 'message':"Import will proceed"}}
+        elif action == 'begin':
+            importer.begin()
+            return {'code':202, 'json':{'ok':True, 'message':"Import will proceed"}}
+        elif action == 'cancel':
+            importer.cancel()
+            return {'code':202, 'json':{'ok':True, 'message':"Import is cancelling"}}
+        elif action == 'finish':
+            importer.finish()
+            del self.importers[source_id]
+            return {'code':200, 'json':{'ok':True, 'message':"Import finished"}}
+        else:
+            return {'code':404, 'json':{'error':True, 'reason':"Unknown action"}}
+    
+    
+    def process_folder_POST(self, req, subpath):
+        source_id, action = subpath[:2]
+        folder = req['utility']['folder']
+        config = self.configfile
+        folders = config.setdefault('sources', {}).setdefault(source_id, {}).setdefault('folders', {})
+        folder_info = folders.setdefault(folder, {})
         
-        #elif True:
-        #    return {'body': "<h1>Hello World!</h1>\n<pre>%s</pre>" % json.dumps(req, indent=4)}
-        elif req['method'] == 'GET':
-            source_id = req['path'][2]
-            if source_id not in self.importers:
-                return {'code':404, 'json':{'error':True, 'reason':"No import is in progress for '%s'." % source_id}}
-            
-            status = self.importers[source_id].status()
-            return {'json':status}
-        
-        return {'code':400, 'json':{'error':True, 'reason':"Bad request"}}
-        
+        if action == 'check':
+            pass
+        elif action == 'allow' or (action == 'update' and folder_info):
+            folder_info['utility'] = req['utility']['name']
+            folder_info['token'] = req['utility']['token']
+            self.configfile = config
+        elif action == 'disable':
+            del folders[folder]
+            self.configfile = config
+            folder_info = None
+        return {'json':{'ok':True, 'allows_originals':bool(folder_info)}}
 
 if __name__ == "__main__":
     LocalHelper().run()
