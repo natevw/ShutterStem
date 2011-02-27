8df613797d8a9adf1a2f6993dbf65b9d4e6a6278
Nathan Vander Wilt <nate@calftrail.com>
2011-02-26 17:18:58 -0800

    got export utility and iframe content playing nicely together

---
+++ shows/export.js
@@ -0,0 +1,10 @@
+function (doc, req) {
+    var ddoc = this;
+    provides("html", function () {
+        doc || (doc = {});
+        doc.database_url = "http://localhost:5984/" + req.info.db_name;
+        doc.utility_path = req.query.utility;
+        doc.csrf_token = req.query.token;
+        return require("lib/flatstache").to_html(ddoc.templates.export, doc);
+    });
+}
---
+++ shows/utility.js
@@ -0,0 +1,27 @@
+function (doc, req) {
+    var ddoc = this;
+    provides("html", function () {
+        if (req.query.random) {
+            // avoid caching of csrf_token (a bit overzealous...?)
+            return {code:400, body:"<h1>Bad Request</h1>You must include a random query parameter."};
+        }
+        
+        doc || (doc = {});
+        var host = "http://localhost:5984/";
+        doc.database_url = host + req.info.db_name;
+        doc.database_futon_url = host + "_utils/database.html?" + req.info.db_name;
+        doc.csrf_token = '' + Math.round(Math.random() * 1e16) + req.uuid + Math.round(Math.random() * 1e16) + '';
+        
+        if (req.query.type === 'export') {
+            doc.title = "Export"
+            doc.location_html = "your desired export destination";
+            doc.frame_host = host;
+            doc.frame_path = req.info.db_name + "/_design/shutterstem/_show/export?token=" + doc.csrf_token + "&utility=";
+            doc.frame_message = req.query.images;
+        }
+        
+        var html = require("lib/flatstache").to_html(ddoc.templates.utility, doc);
+        //return {headers:{'Content-Disposition':"attachment"}, body:html};
+        return html;
+    });
+}
