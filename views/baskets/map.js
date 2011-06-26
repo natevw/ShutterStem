fc97500c96314259922465ef09bf90988c774fdf
Nathan Vander Wilt <nate@calftrail.com>
2011-06-25 21:52:23 -0700

    setting up for another round

--- views/baskets/map.js
+++
@@ -1,6 +0,0 @@
-function (doc) {
-    var BASKET_TYPE = "com.shutterstem.basket"
-    if (!doc[BASKET_TYPE]) return;
-    
-    emit(doc.created, doc.name);
-}
--- views/image_sources/map.js
+++
@@ -1,6 +0,0 @@
-function (doc) {
-    var IMAGE_SOURCE_TYPE = "com.shutterstem.image-source"
-    if (!doc[IMAGE_SOURCE_TYPE]) return;
-    
-    emit(doc.modified || doc.created, doc.name);
-}
