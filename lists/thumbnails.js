5aca842fef254b4ec4c15587f451a69b797d9583
Nathan Vander Wilt <nate@calftrail.com>
2011-07-11 23:43:43 -0700

    bringing back the _list-based thumbnail page (but doesn't actually use view results yet)

--- lists/thumbnails.js
+++ lists/thumbnails.js
@@ -1,6 +1,10 @@
 function (head, req) {
     var ddoc = this;
+    var fs = require('lib/flatstache');
     provides("html", function () {
-        
+        return fs.to_html(ddoc.templates.thumbnails, {
+            app_url: fs.to_html("/{{{db}}}/{{{id}}}", {db:req.info.db_name, id:ddoc._id}),
+            //debug: JSON.stringify({ddoc:ddoc, head:head, req:req}, null, 4)
+        });
     });
 }
