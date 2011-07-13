eae7cef3a70f4b08e862fec10f721841652ab705
Nathan Vander Wilt <nate@calftrail.com>
2011-07-12 22:00:12 -0700

    thumbnail HTML now generated server-side, updated client-side

--- lists/thumbnails.js
+++ lists/thumbnails.js
@@ -2,8 +2,19 @@ function (head, req) {
     var ddoc = this;
     var fs = require('lib/flatstache');
     provides("html", function () {
+        var app_url = fs.to_html("/{{{db}}}/{{{id}}}", {db:req.info.db_name, id:ddoc._id});
+        
+        var photoRow,
+            thumbnails = "",
+            thumbnailTemplate = "<div class='frame' id='{{id}}'><img class='image' src='{{app_url}}/../../{{id}}/thumbnail/512.jpg'></div>";
+        while (photoRow = getRow()) {
+            thumbnails += fs.to_html(thumbnailTemplate, {app_url:app_url, id:photoRow.id});
+        }
+        
+        
         return fs.to_html(ddoc.templates.thumbnails, {
-            app_url: fs.to_html("/{{{db}}}/{{{id}}}", {db:req.info.db_name, id:ddoc._id}),
+            app_url: app_url,
+            thumbnails: thumbnails,
             //debug: JSON.stringify({ddoc:ddoc, head:head, req:req}, null, 4)
         });
     });
