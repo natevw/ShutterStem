d2e68123d2a6c743a1a7ff844a4a1c97b6c0a604
Nathan Vander Wilt <nate@calftrail.com>
2011-09-03 22:10:47 -0700

    tracklogs (<1mb, see https://issues.apache.org/jira/browse/COUCHDB-822) now displayed on map with some ugly visual variability for time/ele

--- updates/convert_gpx.js
+++ updates/convert_gpx.js
@@ -1,15 +1,16 @@
 function (doc, req) {
-    default xml namespace = "http://www.topografix.com/GPX/1/0";
-    req.body = req.body.replace("<?xml version=\"1.0\"?>", "");  // see https://developer.mozilla.org/en/e4x
-    
+    log("HERE 0");
+    req.body = req.body.replace(/^<\?xml .*?>/, "");    // see https://developer.mozilla.org/en/e4x
+    log("HERE 1");
     var file = new XML(req.body),
         locs = [];
-    for each (p in file..trkpt) {
+    log("HERE 2");
+    for each (p in file..*::trkpt) {
         locs.push({
             lat: Number(p.@lat),
             lon: Number(p.@lon),
-            ele: Number(p.ele.text()),
-            time: String(p.time.text())
+            ele: Number(p.*::ele.text()),
+            time: String(p.*::time.text())
         });
     }
     return [null, {json:locs}];
