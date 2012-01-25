eda23632a3d382324d642af2b89a8991d5a974f7
Nathan Vander Wilt <nate@calftrail.com>
2012-01-24 22:31:52 -0800

    now showing geotagged photo counts in their average location instead of tile center

--- views/geotagged/reduce.js
+++ views/geotagged/reduce.js
@@ -1,1 +1,14 @@
-_count
+function (keys, values, redux) {
+    var s = values.reduce((redux) ? function (summed, avg) {
+        summed.lat += avg.lat * avg.count;
+        summed.lon += avg.lon * avg.count;
+        summed.n += avg.count;
+        return summed;
+    } : function (summed, loc) {
+        summed.lat += loc.lat;
+        summed.lon += loc.lon;
+        summed.n += 1;
+        return summed;
+    }, {lat:0, lon:0, n:0});
+    return {lat:s.lat/s.n, lon:s.lon/s.n, count:s.n};
+}
